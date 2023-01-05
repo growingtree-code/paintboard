7ae8ff09e8c9f586c3974c3ceae7f0be1ed0fde7
growingtree-code <tjsruddks12@gmail.com>
2023-01-05 23:32:01 +0900

    understanding shortcut func

--- app.js
+++ app.js
@@ -4,14 +4,8 @@ const ctx = canvas.getContext("2d")
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.rect(50,50,100,100);//x,y = start point w,h = width,height
-ctx.rect(150,150,100,100);
-ctx.rect(250,250,100,100);
-ctx.fill();
-
-ctx.beginPath();//기존에 그린 것과 경로를 끊고 새로시작
-ctx.rect(350,350,100,100);
-ctx.fillStyle = 'red';
-setTimeout(
-    ()=>{ctx.fill();},
-    3000);//test
+ctx.moveTo(50,50);//브러쉬 시작위치
+ctx.lineTo(150,50);//도착할 좌표위치 결록적으로 100 길이에 사각형이그려짐
+ctx.moveTo(150,50);//브러쉬 시작위치
+ctx.lineTo(150,150);
+ctx.stroke();
