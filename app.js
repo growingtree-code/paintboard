b043003e72e6f4bc458e6dc3c468858a34553867
growingtree-code <tjsruddks12@gmail.com>
2023-01-08 15:21:28 +0900

    drawing simple house done

--- app.js
+++ app.js
@@ -6,5 +6,13 @@ canvas.height = 800;
 
 ctx.fillRect(200,200,50,200);
 ctx.fillRect(400,200,50,200);
-ctx.lineWidth= 10;//스타일적용후에 적용된 스타일로 그림을 그려야 화면에 보임
+ctx.lineWidth= 10;
 ctx.strokeRect(300,295,50,100);
+ctx.fillRect(200,200,200,20);
+
+ctx.moveTo(200,200);
+ctx.lineTo(325,100);
+ctx.lineTo(450,200);
+ctx.fill();
+
+
