9d11f4e449b5d8d7df55d84943849a3cba2ae87e
growingtree-code <tjsruddks12@gmail.com>
2023-01-08 15:07:20 +0900

    drawing house ~ing/v2

--- app.js
+++ app.js
@@ -6,4 +6,5 @@ canvas.height = 800;
 
 ctx.fillRect(200,200,50,200);
 ctx.fillRect(400,200,50,200);
-ctx.strokeRect(300,300,50,100);
+ctx.lineWidth= 10;//스타일적용후에 적용된 스타일로 그림을 그려야 화면에 보임
+ctx.strokeRect(300,295,50,100);
