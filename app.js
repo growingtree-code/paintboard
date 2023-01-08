aa02b90348153ce95643a32cfe74c7959b6306c4
growingtree-code <tjsruddks12@gmail.com>
2023-01-09 06:42:40 +0900

    code:draw to user want

--- app.js
+++ app.js
@@ -2,35 +2,23 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d")
 canvas.width = 800;
 canvas.height = 800;
-
-ctx.lineWidth=2;
-const colors = [
-    "#ff3838",
-    "#ffb8b8",
-    "#c56cf0",
-    "#ff9f1a",
-    "#fff200",
-    "#32ff7e",
-    "#7efff5",
-    "#18dcff",
-    "#7d5fff",
-];
-
-function onClick(event) {
-    console.log('onClick func excute',event);
-    ctx.moveTo(0,0);
-    ctx.lineTo(event.offsetX,event.offsetY);
-    ctx.stroke();
+ctx.lineWidth = 2;
+let isPainting = false;
+function callMoveto(event) {
+    if (isPainting){
+        ctx.lineTo(event.offsetX,event.offsetY);
+        ctx.stroke();
+        return;
+    }
+    ctx.moveTo(event.offsetX,event.offsetY);
+}
+function drawstart(event) {
+    isPainting = true;
+}
+function drawend(event) {
+    isPainting = false;
 }
 
-function mousemove(event) {
-    console.log('mousemove',event);
-    ctx.beginPath();
-    ctx.moveTo(Math.random()*800,Math.random()*800);
-    const color = colors[Math.floor(Math.random()*colors.length)];
-    ctx.strokeStyle= color;
-    ctx.lineTo(event.offsetX,event.offsetY);
-    ctx.stroke();
-}
-
-canvas.addEventListener("mousemove",mousemove);
+canvas.addEventListener("mousemove",callMoveto);
+canvas.addEventListener("mousedown",drawstart);
+canvas.addEventListener("mouseup",drawend);
