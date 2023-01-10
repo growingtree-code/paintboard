0481271d1432abca04bc5760dcef797703cba909
growingtree-code <tjsruddks12@gmail.com>
2023-01-10 23:41:42 +0900

    2.2 edit Line Width v1

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d")
 canvas.width = 800;
 canvas.height = 800;
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 let isPainting = false;
 function callMoveto(event) {
     if (isPainting){
@@ -18,8 +19,16 @@ function startPainting(event) {
 function cancelPainting(event) {
     isPainting = false;
 }
+function onLineWidthChange(event) {
+    console.log(event.target.value);
+    ctx.beginPath();
+    // ctx.lineWidth = lineWidth.value;
+    ctx.lineWidth = event.target.value;
+}
 
 canvas.addEventListener("mousemove",callMoveto);
 canvas.addEventListener("mousedown",startPainting);
 canvas.addEventListener("mouseup",cancelPainting);
 canvas.addEventListener("mouseleave",cancelPainting);
+
+lineWidth.addEventListener("change",onLineWidthChange)
